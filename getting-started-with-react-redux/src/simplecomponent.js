faa05cb807b98c2abbd055e281b76d78946772d3
ezrqnkemboi <ezrqnkemboi@gmail.com>
2019-03-03 06:49:57 +0300

    feature(presentational-componenent): add presentation component

---
+++ getting-started-with-react-redux/src/simplecomponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const simpleComponent = () => {
+    return (
+        <div>
+            <h1>Getting started with React Redux</h1>
+            <p>This is a web page created with simple react redux</p>
+        </div>
+    )
+}
+
+export default simpleComponent;
